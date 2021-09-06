7c9ab333e9d7ac566ebc52784af816c937c0aa67
nikeda <4104cafeaulait@gmail.com>
2021-09-07 00:25:15 +0900

    M:スタイル崩れ

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -74,9 +74,11 @@
         </table>
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-4 offset-md-4">
-        {{$books->links()}}
+    <div class="card-body">
+      <div class="row">
+        <div class="col-md-4 offset-md-4">
+          {{$books->links()}}
+        </div>
       </div>
     </div>
     @endif
