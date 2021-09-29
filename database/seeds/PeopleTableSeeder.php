87958d4c0e7575daf684f30fcbcbea9854309908
nikeda <4104cafeaulait@gmail.com>
2021-09-29 13:11:32 +0900

    AM:seeder

---
+++ database/seeds/PeopleTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PeopleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+          'name' => 'xxx',
+          'mail' => 'sss@sss',
+          'age' => 21,
+        ];
+
+        DB::table('people')->insert($param);
+
+        $param = [
+          'name' => 'yyy',
+          'mail' => 'www@www',
+          'age' => 25,
+        ];
+
+        DB::table('people')->insert($param);
+
+        $param = [
+          'name' => 'eee',
+          'mail' => 'eee@eee',
+          'age' => 44,
+        ];
+
+        DB::table('people')->insert($param);
+
+        $param = [
+          'name' => 'rrr',
+          'mail' => 'rrr@rrr',
+          'age' => 32,
+        ];
+
+        DB::table('people')->insert($param);
+    }
+}
+
